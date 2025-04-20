9bbd6ba9295a7e3b7ffeb78a9fdee5694207559a
Priyanshu078 <priyanshupaliwal078@gmail.com>
2025-04-20 12:24:39 +0530

    updated workflow and added tests and some mistakes to check the workflow

---
+++ test/counter_test.dart
@@ -0,0 +1,24 @@
+import 'package:cicd_learn/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("counter tests", () {
+    late final Counter counter;
+    setUp(() {
+      counter = Counter();
+    });
+    test("initial value of counter is 0", () {
+      expect(counter.counter, 0);
+    });
+
+    test("when incremented the counter should increment to 1", () {
+      counter.increment();
+      expect(counter.counter, 1);
+    });
+
+    test("when decremented the counter should decrement to -1", () {
+      counter.decrement();
+      expect(counter.counter, -1);
+    });
+  });
+}
